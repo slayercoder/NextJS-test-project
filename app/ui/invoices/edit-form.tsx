cbebad771de1a340a1a98afb26491bd5732ec65a
slayercoder <ms81291@gmail.com>
2025-06-29 11:06:38 +0530

    NextJS chapters completion

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +18,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = {errors: {}, message: null};
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -31,6 +36,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-labelledby='customer-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -42,6 +48,13 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error">
+            {
+              state.error?.customerId && state.error.customerId.map((error: string) => {
+                return <p className='mt-2 text-sm text-red-500' key={error}>{error}</p>
+              })
+            }
           </div>
         </div>
 
@@ -60,9 +73,17 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-labelledby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error">
+            {
+              state.error?.amount && state.error.amount.map((error: string) => {
+                return <p className='mt-2 text-sm text-red-500' key={error}>{error}</p>
+              })
+            }
           </div>
         </div>
 
